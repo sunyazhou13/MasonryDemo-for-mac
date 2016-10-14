c21bdf74d0b359db8cc5053e0f58aab2cb64ebef
sunyazhou <sunyazhou@baidu.com>
2016-10-14 12:43:37 +0800

    [Masonry相关][相关资料]
    1. 相关文章资料整理

---
+++ Masonry相关资料.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+//: 官方地址
+
+var masonry官方地址 = "https://github.com/SnapKit/Masonry"
+
+//: 使用纯代码进行iOS应用的autolayout自适应布局
+
+var 中文资料翻译 = "http://www.ios122.com/2015/09/masonry/"
+
+//: 土土哥博客
+
+var 有趣的Autolayout示例Masonry实现 = "http://tutuge.me/2015/05/23/autolayout-example-with-masonry/"
+
+var 有趣的Autolayout示例2Masonry实现 = "http://tutuge.me/2015/08/08/autolayout-example-with-masonry2/"
+
+var 有趣的Autolayout示例3Masonry实现 = "http://tutuge.me/2015/12/14/autolayout-example-with-masonry3/"
+
+var 有趣的Autolayout示例4Masonry实现 = "http://tutuge.me/2016/08/06/autolayout-example-with-masonry4/"
+//: Cocoapods相关
+var cocoapods = "http://blog.devtang.com/2014/05/25/use-cocoapod-to-manage-ios-lib-dependency/"
+//: 自动布局的在viewController里面的相关方法(视图的生命周期)
+var contrainsmethods = "http://web.stanford.edu/class/cs193p/cgi-bin/drupal/node/425"
+
+
+
